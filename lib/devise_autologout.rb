56c1943d2f7ed972b484b082149631aa3470aa90
Artem Kramarenko <me@artemk.name>
2011-12-25 19:52:26 +0200

    initial

---
+++ lib/devise_autologout.rb
@@ -0,0 +1,2 @@
+module DeviseAutologout
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '9a6545d264dba10b928c1700ee0b0e3f3a989add7a0ce25a9ed917c6b4b9de7b85a865d65f560eb25fb8ee72f21d2374b9c2870839c1ba06c486237159fa9412'
